6de04fae2ec8ae6285318295f1c974a5b557619a
hnchen <hnchen@ruc.edu.cn>
2024-06-17 15:07:53 +0800

    demos

--- run_scripts/generate_data/paraphrase.sh
+++ run_scripts/generate_data/paraphrase.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 
 python ./data_augmentation/generating_data.py \
---demo_file="./data/paraphrase_demos.json" \
+--demo_file="./data/demos/paraphrase_demos.json" \
 --eval_file="./data/qrecc/train.json" \
 --output_file="./data/qrecc/paraphrase.jsonl" \
 --model="meta-llama/Llama-2-7b-chat-hf" \
